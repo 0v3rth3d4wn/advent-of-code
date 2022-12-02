ac6e55600e792dd331f9b66c71dc21080f280a63
Ivo Karadzhov <ivo.karadzhov@gmail.com>
2022-12-02 20:47:52 +0200

    Day 2

---
+++ 2022/rust/day-02/src/main.rs
@@ -0,0 +1,56 @@
+use colored::*;
+use std::fs;
+
+fn main() {
+    let data = "data.txt";
+    let game = fs::read_to_string(data).unwrap();
+    let game: Vec<&str> = game.trim().split("\n").collect();
+
+    println!(
+        "Prediction: {}",
+        game.iter()
+            .fold(0, |acc, res| {
+                acc + match *res {
+                    "A X" => 4,
+                    "A Y" => 8,
+                    "A Z" => 3,
+
+                    "B X" => 1,
+                    "B Y" => 5,
+                    "B Z" => 9,
+
+                    "C X" => 7,
+                    "C Y" => 2,
+                    "C Z" => 6,
+
+                    _ => 0,
+                }
+            })
+            .to_string()
+            .green()
+    );
+
+    println!(
+        "Actual: {}",
+        game.iter()
+            .fold(0, |acc, res| {
+                acc + match *res {
+                    "A X" => 3,
+                    "A Y" => 4,
+                    "A Z" => 8,
+
+                    "B X" => 1,
+                    "B Y" => 5,
+                    "B Z" => 9,
+
+                    "C X" => 2,
+                    "C Y" => 6,
+                    "C Z" => 7,
+
+                    _ => 0,
+                }
+            })
+            .to_string()
+            .green()
+    );
+}
