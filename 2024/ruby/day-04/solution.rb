7721f780beb5dc98e279a4b4c266cc7829512ad2
Ivo Karadzhov <ivo.karadzhov@gmail.com>
2024-12-05 22:10:34 +0200

    2024 day-04

--- 2024/ruby/day-04/solution.rb
+++ 2024/ruby/day-04/solution.rb
@@ -1,35 +1,36 @@
-f = File.read('demo.txt').strip.lines.map(&:strip)
-puts f.join('\n').scan(/XMAS/).count
-print "\n"
-puts f.map(&:reverse).join('\n').scan(/XMAS/).count
-print "\n"
-puts f.reverse.join('\n').scan(/XMAS/).count
-print "\n"
-puts f.map(&:chars).transpose.map(&:join).join("\n").scan(/XMAS/).count
-print "\n"
-puts f.map(&:chars).transpose.map(&:join).join("\n").reverse.scan(/XMAS/).count
-print "\n"
-puts f.reverse.map(&:chars).transpose.map(&:join).join("\n").scan(/XMAS/).count
-print "\n"
-puts f.reverse.map(&:chars).transpose.map(&:join).join("\n").reverse.scan(/XMAS/).count
+c = File.readlines('input.txt').map { |line| line.strip.chars }
+f, f_tr, f_d_rev = c.then { |f| [f, f.transpose, f.map(&:reverse)] }
+def inside?(inp, i, j, curr) j + curr <= inp.size - 1 && i + curr <= inp.size - 1 end
+def diag(inp, i, j) [inp[i][j], inp[i + 1][j + 1], inp[i + 2][j + 2], inp[i + 3][j + 3]].join end
+s1 = %w[XMAS SAMX]
+s2 = %w[MAS SAM]
+p1, p2 = 0, 0
 
-# f = File.read('demo.txt').lines.map(&:strip).map(&:chars)
-# width = f.first.length
-# height = f.rotate.first.length
-# print width
-# print height
-# print f
-# f.each
-# forward
-# print f.scan(/XMAS/)
-# print f.reverse.scan(/XMAS/)
-# print f.lines.rotate.join
+f.size.times do |i|
+  f[0].size.times do |j|
+    p1 += 1 if s1.include? f[i][j..j + 3].join
+    p1 += 1 if s1.include? f_tr[i][j..j + 3].join
+    next unless [1, 2, 3].all? { inside?(f, i, j, _1) }
 
-ff = (0..9).collect { |i| f[i][i] }
-print ff
+    p1 += 1 if s1.include? diag(f, i, j)
+    p1 += 1 if s1.include? diag(f_d_rev, i, j)
 
-# f.each do |line|
-#   line.each do |c|
-#     print c
-#   end
-# end
+    d1 = [f[i][j], f[i + 1][j + 1], f[i + 2][j + 2]].join
+    d2 = [f[i + 2][j], f[i + 1][j + 1], f[i][j + 2]].join
+    p2 += 1 if (s2.include? d1) && (s2.include? d2)
+  end
+end
+
+puts p1
+
+f.size.times do |i|
+  f[0].size.times do |j|
+    next unless [1, 2].all? { inside?(f, i, j, _1) }
+
+    d1 = [f[i][j], f[i + 1][j + 1], f[i + 2][j + 2]].join
+    d2 = [f[i + 2][j], f[i + 1][j + 1], f[i][j + 2]].join
+    p2 += 1 if (s2.include? d1) && (s2.include? d2)
+  end
+end
+
+puts p2
